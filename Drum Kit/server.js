9cc6ca27f8aee002a5b1068dc653fdfb491af11a
Shiva Reddy <59078131+shiva-reddy-1001@users.noreply.github.com>
2022-01-28 16:12:26 +0530

    hehe

---
+++ Drum Kit/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(__dirname));
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
